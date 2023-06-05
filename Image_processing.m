e51a0cc9ed34c9301e9e82ca8db169a7dd5004bb
hanchang12345 <120896886+hanchang12345@users.noreply.github.com>
2023-06-05 18:04:10 +0800

    影像处理

---
+++ Image_processing.m
@@ -0,0 +1,62 @@
+%% 自己实现的函数，需要一一遍历
+I=imread('rice.png');
+level=graythresh(I).*255;
+[m, n, k]=size(I);
+for i=1:m
+    for j=1:n
+        if I(i,j)>=level
+            bw(i,j)=1;
+        else
+            bw(i,j)=0;
+        end
+    end
+end
+subplot(1,2,1);
+imshow(I);
+subplot(1,2,2);
+imshow(bw);
+%% 使用编辑器的函数来进行
+I = imread('rice.png');
+bw = imbinarize(I);
+subplot(1,2,1);
+imshow(I);
+subplot(1,2,2);
+imshow(bw);
+%% 自己写的
+I =imread('rice.png');
+BG=imopen(I,strel('disk',15));
+I2=imsubtract(I,BG);% 把BG从I上删去
+bw1=imbinarize(I);
+bw2=imbinarize(I2);
+subplot(1,2,1);
+imshow(I);
+subplot(1,2,2);
+imshow(BG);
+figure;
+subplot(1,2,1);
+imshow(bw1);
+subplot(1,2,2);
+imshow(bw2);
+%% 
+I=imread('rice.png');
+BG=imopen(I, strel('disk', 15));
+I2=imsubtract(I, BG); level=graythresh(I2); 
+BW=imbinarize(I2, level); 
+[labeled, ~]=bwlabel(BW, 8);
+RGB_label=label2rgb(labeled); imshow(RGB_label);
+%%
+I=imread('rice.png'); 
+BG=imopen(I, strel('disk', 15));
+I2=imsubtract(I, BG); level=graythresh(I2); 
+BW=imbinarize(I2, level); 
+[labeled, numObjects]=bwlabel(BW, 8);
+graindata = regionprops(labeled, 'basic');
+graindata(51)
+%%
+I=imread('rice.png'); level=graythresh(I);
+BG=imopen(I, strel('disk', 15));
+I2=imsubtract(I, BG); BW=imbinarize(I2, graythresh(I2)); 
+ObjI = bwselect(BW); imshow(ObjI);
+% 回车显示选中的米粒
+
+
